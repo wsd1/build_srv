8ee60318b7841d8e65d5732e1213d9b53b32e19b
ws_d1 <ws_dy@sina.com>
2014-05-19 00:07:14 +0800

    add snippet collection

---
+++ test_snippet/demo_local_build.js
@@ -0,0 +1,25 @@
+﻿var spawn = require('child_process').spawn;
+
+		var src = 'ir.c';
+		var out =  src.split('.')[0] + '.add';
+		var params = [];
+		params.push(/tmp/' + out);
+		params.push('/usr/local/src/build_srv/addon_sdk_v1');
+		params.push('/usr/local/src/build_srv/addon_sdk_v1/addlite.ld');
+		params.push('/tmp/' + src);
+
+		console.log(params);
+
+		builder = spawn('/usr/local/src/build_srv/build.sh', params);
+		
+		builder.stdout.on('data', function (data) {
+		console.log('stdout: ' + data);
+		});
+
+		builder.stderr.on('data', function (data) {
+		console.log('stderr: ' + data);
+		});
+		
+		builder.on('close', function (code) {
+		console.log('child process exited with code ' + code);
+		});
